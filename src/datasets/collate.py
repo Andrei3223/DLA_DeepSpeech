9331280937b5683bc7ae115f71880c6aeb968986
Andrei3223 <a.gruzitski@gmail.com>
2024-10-05 18:57:45 +0300

    collate

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,5 +1,7 @@
 import torch
 
+from torch.nn.utils.rnn import pad_sequence
+
 
 def collate_fn(dataset_items: list[dict]):
     """
@@ -14,4 +16,17 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    pass  # TODO
+    result_batch = {}
+
+    result_batch["text"] = [item["text"] for item in dataset_items]
+    result_batch["text_encoded"] = pad_sequence([item["text_encoded"].squeeze(0) for item in dataset_items], batch_first=True)
+    result_batch["text_encoded_length"] = torch.tensor([item["text_encoded"].shape[1] for item in dataset_items])
+
+    result_batch["audio_path"] = [item["audio_path"] for item in dataset_items]
+    result_batch["audio"] = pad_sequence([item["audio"].squeeze(0) for item in dataset_items], batch_first=True)
+
+    result_batch["spectrogram"] = pad_sequence([item["spectrogram"].permute(2, 1, 0) for item in dataset_items], batch_first=True).squeeze(-1).permute(0, 2, 1)
+    result_batch["spectrogram_length"] = torch.tensor([item["spectrogram"].shape[2] for item in dataset_items])
+
+    # raise RuntimeError(result_batch["spectrogram_length"])
+    return result_batch
--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,16 @@
-# Based on seminar materials
+import editdistance
 
-# Don't forget to support cases when target_text == ''
+def calc_wer(target_text: str, pred_text: str):
+    if len(target_text) == 0:
+        if pred_text != '':
+            return 1.
+        return 0.
+    return editdistance.eval(target_text.split(), pred_text.split()) / len(target_text.split())
+    
 
-
-def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
-
-
-def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+def calc_cer(target_text: str, pred_text: str):
+    if len(target_text) == 0:
+        if pred_text != '':
+            return 1.
+        return 0.
+    return editdistance.eval(target_text, pred_text) / len(target_text)
