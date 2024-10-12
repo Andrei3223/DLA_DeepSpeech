1e352f6247170c8acdad6a1da6e072437c6b7b86
Andrei3223 <a.gruzitski@gmail.com>
2024-10-12 22:18:47 +0300

    data preproc

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -84,7 +84,9 @@ class BaseDataset(Dataset):
         text = data_dict["text"]
         text_encoded = self.text_encoder.encode(text)
 
-        spectrogram = self.get_spectrogram(audio)
+        instance_data = {"audio": audio}
+        instance_data = self.preprocess_data(instance_data)
+        spectrogram = self.get_spectrogram(instance_data["audio"])
 
         instance_data = {
             "audio": audio,
@@ -93,11 +95,6 @@ class BaseDataset(Dataset):
             "text_encoded": text_encoded,
             "audio_path": audio_path,
         }
-
-        # TODO think of how to apply wave augs before calculating spectrogram
-        # Note: you may want to preserve both audio in time domain and
-        # in time-frequency domain for logging
-        instance_data = self.preprocess_data(instance_data)
 
         return instance_data
 
