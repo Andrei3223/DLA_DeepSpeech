1d5e587897af91bf328ed44fd5d2a0c76906ad8b
Andrei3223 <a.gruzitski@gmail.com>
2024-10-12 12:18:59 +0300

    augmentations

--- src/transforms/spec_augs/masks.py
+++ src/transforms/spec_augs/masks.py
@@ -1,21 +1,22 @@
-from torch import Tensor, nn
+from torch import Tensor, nn, rand
 import torchaudio.transforms
 
 
 class FreqMasking(nn.Module):
-    def __init__(self, freq_mask_param):
+    def __init__(self, freq_mask_param, prob=1):
         super().__init__()
         self.transform = torchaudio.transforms.FrequencyMasking(freq_mask_param)
+        self.apply_prob = prob
 
     def __call__(self, data: Tensor):
-        return self.transform(data)
+        return data if rand(1) < self.apply_prob else self.transform(data)
         
 
 
 class TimeMasking(nn.Module):
-    def __init__(self, time_mask_param):
+    def __init__(self, time_mask_param, prob=1):
         super().__init__()
         self.transform = torchaudio.transforms.TimeMasking(time_mask_param)
 
     def __call__(self, data: Tensor):
-        return self.transform(data)
+        return data if rand(1) < self.apply_prob else self.transform(data)
